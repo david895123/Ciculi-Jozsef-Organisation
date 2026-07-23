6bf095148303bfe306785ec1ff9487a54afed1aa
david895123 <davidmihaly895@gmail.com>
2026-07-23 16:43:56 +0300

    change language loader path

--- Frontend/js/i18n.js
+++ Frontend/js/i18n.js
@@ -1,5 +1,5 @@
 async function loadLanguage(lang) {
-  const res = await fetch(`/frontend/lang/${lang}.json`);
+  const res = await fetch(`lang/${lang}.json`);
   const dict = await res.json();
   document.querySelectorAll('[data-i18n]').forEach(el => {
     const key = el.getAttribute('data-i18n');
